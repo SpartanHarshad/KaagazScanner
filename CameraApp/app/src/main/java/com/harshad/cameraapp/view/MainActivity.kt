0d979a885320761d275d603769005bb92093b0da
SpartanHarshad <harshadpawar8555@gmail.com>
2021-07-31 14:02:59 +0530

    initial commit

---
+++ CameraApp/app/src/main/java/com/harshad/cameraapp/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.harshad.cameraapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.harshad.cameraapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
